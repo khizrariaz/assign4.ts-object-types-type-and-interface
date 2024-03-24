0344b487cde4fe4717c147f2a9f247b73fde375a
khizrariaz <khizrariaz@gmail.com>
2024-03-24 13:40:09 +0500

    my 1st commit
    assignment 4. object n array

---
+++ assign4.js
@@ -0,0 +1,16 @@
+//type and interf are 2types of object used to give name and type during coding
+//let bike : biketype = {
+let bike = {
+    company: 'Honda',
+    color: ['black', 'silver', 'white'],
+    wheels: 2,
+    type: 'heavy',
+    engine: '700cc',
+    brandDetails: {
+        price: "1lac",
+        origin: "branded",
+        manufacturer: "Pakistani"
+    }
+};
+console.log(bike);
+export {};
